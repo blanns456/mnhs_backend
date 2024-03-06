ead13a764c7ab8633870e3d9a077fdf03eb0bfac
JBHtzy <jamesbadang16@gmail.com>
2024-03-06 14:58:30 -0800

    retrieving data input

--- example-app/app/Http/Controllers/StudentPersonalInfoController.php
+++ example-app/app/Http/Controllers/StudentPersonalInfoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StudentPersonalInfo;
 use Illuminate\Http\Request;
 
 class StudentPersonalInfoController extends Controller
--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\StudentPersonalInfoController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
